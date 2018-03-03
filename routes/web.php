877d7aa640c0ecd8385ecec32428b30075fb7395
Your Name <you@example.com>
2018-03-03 09:19:03 -0800

    1

--- routes/web.php
+++ routes/web.php
@@ -36,9 +36,12 @@ Route::get('/baseball', function () {
     curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
     curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
     curl_setopt($ch, CURLOPT_USERPWD, "$login:$password");
-    $json = curl_exec($ch);
+    $data = curl_exec($ch);
     curl_close($ch);  
-    echo($json); 
+    //echo($json); 
+
+
+    $json = json_decode($data,true);
 
 
     //echo( $json['latestupdates']['lastUpdatedOn']);
