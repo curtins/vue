c5d12028b1b359b5b362e2bd782e50dc12ab786a
Your Name <you@example.com>
2018-03-04 07:30:52 -0800

    1

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::get('/baseball', function () {
 
     $json = json_decode($data,true); 
 
-    foreach ($arr as $key => $value) {
+    foreach $json as $key => $value) {
         echo $key;
        }
 
